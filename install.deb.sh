cdfd8806a2d4cba0439d41c96d08323b237a9693
hastmu <gh-hastmu@gmx.de>
2024-11-03 12:10:23 +0100

    test

--- install.deb.sh
+++ install.deb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# defaults
-echo "- BRANCH [${BRANCH:=@branch@}]"
+# defaults 
+echo "- BRANCH [${BRANCH:=feature/12-establish-pre-hook-for-md-updates}]"
 NAME="apt-proxy-detect"
 VERSION="1.0.0"
 HEADHASH="$(date +%s)"
